1f41e041c9522c39ba2c2c3c7c20d3107b9edf18
JohnKouf99 <johnkouf1999@gmail.com>
2025-02-25 19:53:23 +0200

    added web harvesting scripts

---
+++ reportparse/web_rag/pipeline.py
@@ -0,0 +1,40 @@
+import os
+from reportparse.web_rag.search import google_search
+from reportparse.web_rag.harverst import Harvester
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class pipeline:
+
+    def __init__(self, query, n):
+        self.query = query
+        self.n = n
+        self.harvested_urls= None
+
+    #based on a claim, implement a searcher and a harvester 
+    def retrieve_knowledge(self):
+
+        #scan the web for urls containing knowledge
+        url_list = google_search(self.query, self.n + 2)
+        if url_list is None:
+            print('Could not find any results regarding the claim. Please try again or choose a different statement')
+            return None
+        else:
+            #harvest the external urls using a harvester instance
+            my_harvester = Harvester(list(url_list), self.query, timeout=1000, claim_id=0, max_sources = self.n)
+            df = my_harvester.run()
+            #df.to_csv('result_df.csv', index=False)
+            #get the bodies of the top-n web sources that has the biggest "body_similarity" value
+            try:
+                result = df.nlargest(self.n, 'body_similarity')['similar_sentences'] 
+                self.harvested_urls = df.nlargest(self.n, 'body_similarity')['url'].to_list()
+            except Exception as e:
+                print('Could not find relevant sources.')
+                return None
+        
+        return result
+    
+
+pip = pipeline('Toyota factory is harming the enviroment', 3)
+result = pip.retrieve_knowledge()
+print(print("\n".join(result.astype(str))) )
