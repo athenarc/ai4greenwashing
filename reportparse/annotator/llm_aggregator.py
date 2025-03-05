e1ab17f88b6b8a01e396c5b880ec6251b6cfefe9
Geoka1 <geoka001@gmail.com>
2025-03-05 15:13:30 +0200

    added aggregator llm

--- reportparse/annotator/llm_aggregator.py
+++ reportparse/annotator/llm_aggregator.py
@@ -6,9 +6,12 @@ from reportparse.annotator.chroma_annotator import LLMAnnotator
 import argparse
 import re
 import json
-from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-
+from logging import getLogger
+from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
+import os
+logger = getLogger(__name__)
 
 @BaseAnnotator.register("llm_agg")
 class LLMAggregator(BaseAnnotator):
@@ -17,7 +20,81 @@ class LLMAggregator(BaseAnnotator):
         load_dotenv()
         self.web = WEB_RAG_Annotator()
         self.chroma = LLMAnnotator()
+        if os.getenv("USE_GROQ_API") == "True":
+            self.llm = ChatGroq(
+                model=os.getenv("GROQ_LLM_MODEL_1"),
+                temperature=0,
+                max_tokens=None,
+                timeout=None,
+                max_retries=1,
+                groq_api_key=os.getenv("GROQ_API_KEY_1"),
+            )
+
+            self.llm_2 = ChatGroq(
+                model=os.getenv("GROQ_LLM_MODEL_2"),
+                temperature=0,
+                max_tokens=None,
+                timeout=None,
+                max_retries=1,
+                groq_api_key=os.getenv("GROQ_API_KEY_2"),
+            )
+        else:
+            self.llm = ChatOllama(model=os.getenv("OLLAMA_MODEL"), temperature=0)
+            self.llm_2 = ChatOllama(model=os.getenv("OLLAMA_MODEL"), temperature=0)
         return
+
+    def call_aggregator(self, claim, chroma_result, web_rag_result):
+        messages = [
+            (
+                "system",
+                f"""You have at your disposal two independent verdicts regarding the accuracy of a given statement: '[User Input]'.  
+
+                - The first verdict (Database Verdict) is derived from an LLM that has access to a structured database containing the entire document from which the statement was extracted.  
+                - The second verdict (Web Verdict) is derived from an LLM that retrieves and analyzes information from the web to assess the claim.  
+
+                Your task is to analyze both verdicts and reach a final conclusion regarding the statement's accuracy.  
+
+                Use only the provided information in combination with your knowledge to decide whether the statement is TRUE, FALSE, PARTIALLY TRUE, or PARTIALLY FALSE.  
+
+                Before making your final decision:  
+
+                1. Analyze the given statement clearly to identify its key elements.  
+                2. Examine the reasoning in both the Database Verdict and the Web Verdict.  
+                3. Compare both verdicts and resolve any discrepancies by determining which source provides stronger, more reliable justification.  
+                4. Use your own reasoning to synthesize the evidence and reach a final, well-supported conclusion.  
+
+                Possible Results:  
+                - TRUE If both sources fully confirm the statement, or if one provides strong confirmation while the other lacks contradictory evidence.  
+                - FALSE: If both sources clearly disprove the statement, or if one strongly refutes it while the other is inconclusive.  
+                - PARTIALLY TRUE: If the statement contains correct elements but is incomplete or slightly misleading.  
+                - PARTIALLY FALSE: If the statement has some correct elements but is also significantly inaccurate or misleading.  
+
+                Finally, explain your reasoning clearly and focus on the provided data and your own knowledge. Avoid unnecessary details and try to be precise and concise in your analysis. Your answers should be in the following format:
+                
+                Statement: '[User Input]'  
+                Final Aggregated Verdict:  
+                [Your final decision based on the provided evidence]  
+                Justification:  
+                [Your reasoning, synthesizing the database and web results while maintaining precision and conciseness.]  
+                """,
+            ),
+            (
+                "human",
+                f"""Statement: {claim}  
+                Database Verdict: {chroma_result}  
+                Web Verdict: {web_rag_result}  
+                """,
+            ),
+        ]
+        try:
+            logger.info("Calling LLM to verify claim with context")
+            ai_msg = self.llm.invoke(messages)
+            print("AI message: ", ai_msg.content)
+            return ai_msg.content
+        except Exception as e:
+            logger.error(f"Error calling LLM: {e}")
+            return "Error: Could not generate a response."
+
 
     def annotate(
         self,
@@ -99,7 +176,7 @@ class LLMAggregator(BaseAnnotator):
                     )
                     print("Second llm result: ", chroma_result)
                     # annotate for chroma
-                    claim_dict = {
+                    claim_dict_chroma = {
                         "claim": c,
                         "retrieved_pages": retrieved_pages,
                         "Label": self.chroma.extract_label(chroma_result),
@@ -107,7 +184,7 @@ class LLMAggregator(BaseAnnotator):
                             chroma_result
                         ),
                     }
-                    json_output = json.dumps(claim_dict)
+                    json_output = json.dumps(claim_dict_chroma)
                     _annotate(
                         _annotate_obj=page,
                         _text=chroma_result,
@@ -117,13 +194,13 @@ class LLMAggregator(BaseAnnotator):
 
                     # add web_rag aggregation
                     web_rag_result, url_list = self.web.web_rag(c, web_sources=1)
-                    claim_dict = {
+                    claim_dict_webrag = {
                         "claim": c,
                         "urls": url_list,
                         "Label": self.web.extract_label(web_rag_result),
                         "Justification": self.web.extract_justification(web_rag_result),
                     }
-                    json_output = json.dumps(claim_dict)
+                    json_output = json.dumps(claim_dict_webrag)
 
                     # annotate for web rag
                     _annotate(
@@ -133,6 +210,24 @@ class LLMAggregator(BaseAnnotator):
                         metadata=json_output,
                     )
 
+                    aggregator_result = self.call_aggregator(c, chroma_result, web_rag_result)
+                    print("Aggregator result: ", aggregator_result)
+                    _annotate(
+                        _annotate_obj=page,
+                        _text=aggregator_result,
+                        annotator_name="aggregator_result",
+                        metadata=json.dumps(
+                            {
+                                "claim": c,
+                                "chroma_result": chroma_result,
+                                "web_rag_result": web_rag_result,
+                                "Label": self.web.extract_label(aggregator_result),
+                                "Justification": self.web.extract_justification(
+                                    aggregator_result
+                                ),
+                            }
+                        ),
+                    )
                 gw_index += 1
 
             else:
