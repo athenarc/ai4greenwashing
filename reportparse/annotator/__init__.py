d3067ec030609d06cba33a49871b0145403cb592
gkaoukis <geoka001@gmail.com>
2025-11-29 12:29:42 +0200

    clean

--- reportparse/annotator/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for module in os.listdir(os.path.dirname(__file__)):
-    if module == '__init__.py' or module[-3:] != '.py':
-        continue
-    __import__('reportparse.annotator.' + module[:-3], locals(), globals())
--- reportparse/util/settings.py
+++
@@ -1,4 +0,0 @@
-from typing import Set
-
-LEVEL_NAMES: Set[str] = {'page', 'block', 'sentence', 'text'}
-LAYOUT_NAMES: Set[str] = {'title', 'text', 'list', 'cell'}
